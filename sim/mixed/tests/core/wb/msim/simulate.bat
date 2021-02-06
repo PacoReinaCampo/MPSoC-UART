9b60042177700699429c8c1ee4ae10175e43daac
PacoReinaCampo <frareicam@gmail.com>
2021-02-06 20:33:59 +0100

    Working in Peripheral-UART

--- sim/mixed/tests/core/wb/msim/simulate.bat
+++ sim/mixed/tests/core/wb/msim/simulate.bat
@@ -1,6 +1,6 @@
 call ../../../../../../settings64_msim.bat
 
 vlib work
-vlog +incdir+../../../../../../rtl/verilog/wb/pkg -f system.verilog.vc
-vcom -f system.vhdl.vc
+vlog -sv +incdir+../../../../../../rtl/verilog/wb/pkg -f system.verilog.vc
+vcom -2008 -f system.vhdl.vc
 vsim -c -do run.do work.mpsoc_uart_testbench
