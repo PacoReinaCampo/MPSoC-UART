e920d6c3fe5eea04fe40c322c6f21bb5cfcd9292
PacoReinaCampo <frareicam@gmail.com>
2021-02-08 13:56:08 +0100

    Working in Peripheral-UART

--- synthesis/verilog/wb/vivado/synthesize.bat
+++ synthesis/verilog/wb/vivado/synthesize.bat
@@ -3,7 +3,7 @@ REM #################################################################
 REM # Copyright (c) 1986-2020 Xilinx, Inc.  All rights reserved.    #
 REM #################################################################
 
-call ../../../../../../settings64_vivado.bat
+call ../../../../settings64_vivado.bat
 
 vivado -nojournal -log system.log -mode batch -source system.tcl
 pause
